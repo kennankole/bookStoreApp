cb19c023051102399e45c76b44985ef592d34579
kennankole <kennankole@gmail.com>
2022-12-07 09:28:09 +0300

    Initialized the project with components  - Created the following components     * BookAdd     * BookItem     * BookList     * Categories     * NavigationMenu  - Created different pages and linked them with route

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Category = () => (
+  <section>
+    <div>
+      <button type="submit">Check status</button>
+    </div>
+  </section>
+);
+export default Category;
