e5fb641bc4a7991dec6fe8b1e1416cc5a8d114c1
kennankole <kennankole@gmail.com>
2022-12-13 12:00:05 +0300

    Display data from redux store

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,15 @@
 /* eslint-disable array-callback-return */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
 const BookList = () => {
-  const state = {
-    data: [
-      {
-        title: 'JavaScript ES6',
-        author: 'Ankole',
-        id: 1,
-      },
-      {
-        title: 'React-Redux ',
-        author: 'Anko',
-        id: 2,
-      },
-      {
-        title: 'TDD in React',
-        author: 'Ankoe',
-        id: 3,
-      },
-    ],
-  };
+  const data = useSelector((state) => state.books);
+
   return (
     <section>
       <ul>
-        {state.data.map((item) => (
+        {data.map((item) => (
           <BookItem
             key={item.id}
             data={item}
